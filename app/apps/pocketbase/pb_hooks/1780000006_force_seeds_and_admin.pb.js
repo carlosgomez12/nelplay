a8bfc7c6b49da53ad1f8001abf5438db75f87b7f
carlosgomez12 <126099441+carlosgomez12@users.noreply.github.com>
2026-09-05 18:07:10 -0500

    Delete app directory

--- app/apps/pocketbase/pb_hooks/1780000006_force_seeds_and_admin.pb.js
+++
@@ -1,5 +0,0 @@
-// This file has been deleted - it was incorrectly created as a hook.
-// The correct migration file is now at: pb_migrations/1780000006_force_seeds_and_admin.js
-// This placeholder ensures the hook file is removed from the system.
-
-// DO NOT USE THIS FILE - it will be ignored by PocketBase
--- app/apps/pocketbase/pb_hooks/create_app_secrets_migration.pb.js
+++
@@ -1,3 +0,0 @@
-/// <reference path="../pb_data/types.d.ts" />
-// This is a placeholder - the actual migration file will be created separately
-// Migration: 1780000011_create_app_secrets.js
